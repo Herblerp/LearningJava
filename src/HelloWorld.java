1114ba00e59d50f6e633c61fa6aba6617ac1965e
Herblerp <Herblerpgaming@gmail.com>
2017-09-11 12:14:33 +0200

    Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,22 @@
  */
 public class HelloWorld {
     public static void main(String args[]){
+
+        //Hello world
         System.out.println("Hello world!");
+
+        //Variables
+        int myNumber = 5;
+        System.out.println(myNumber);
+
+        float myFloat = 5f/3f;
+        System.out.println(myFloat);
+
+        double myDouble = 5d/3d;
+        System.out.println(myDouble);
+
+        double pi = Math.PI;
+        System.out.println(pi);
+
     }
 }
